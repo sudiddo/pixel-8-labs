65f0eecebcb56a84a5235e267e9cfb08afb922c3
Stanley Nguyen <hung.ngn.the@gmail.com>
2023-08-27 23:01:13 +0800

    feat: add full requirements

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,23 +1,10 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { WagmiConfig, createClient } from 'wagmi';
-import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 
-const client = createClient(
-  getDefaultClient({
-    appName: 'Project Name',
-    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
-    infuraId: process.env.NEXT_PUBLIC_INFURA_API_KEY,
-  }),
-);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={client}>
-      <ConnectKitProvider>
-        <Component {...pageProps} />
-      </ConnectKitProvider>
-    </WagmiConfig>
+    <Component {...pageProps} />
   );
 }
 
